c4a86313877ca5c80aa177c5631cf56aa3e647ad
shabz35 <67912357+shabz35@users.noreply.github.com>
2024-01-16 14:27:34 +0000

    app redirect

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, redirect, request
 from flask import render_template
 import todo_app
-from todo_app.data.session_items import get_items
+from todo_app.data.session_items import add_item, get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -12,4 +12,10 @@ app.config.from_object(Config())
 def index():
     items = get_items()
     return render_template("index.html", items = items)
+
+@app.route('/add-todo', methods=["post"])
+def add_todo():
+    title = request.form.get('title')
+    add_item(title)
+    return redirect('/')
 
