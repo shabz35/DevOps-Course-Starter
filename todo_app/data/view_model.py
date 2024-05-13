29b6b5cecdc27eb26d1efe7906df22dd4c4bd3e6
shabz35 <67912357+shabz35@users.noreply.github.com>
2024-05-13 11:38:14 +0100

    property list item

--- todo_app/data/view_model.py
+++ todo_app/data/view_model.py
@@ -8,3 +8,21 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+    
+    @property
+    def todo_items(self):
+        return []
+    
+    @property
+    def doing_items(self):
+        return []
+    
+    @property
+    def done_items(self) -> list[Item]: 
+        output = []
+
+        for item in self._items:
+            if item.status == "Done":
+                output.append(item)
+
+        return output
---
+++ todo_app/test_view_module.py
@@ -0,0 +1,24 @@
+from todo_app.data import item
+from todo_app.data.view_model import ViewModel
+
+
+def test_done_items_property_only_shows_done_items_and_nothing_else():
+    #ARRANGE
+    items = [
+        item.Item(1, "Started Todo", "To Do"),
+        item.Item(2, "In Progress Todo", "Doing"),
+        item.Item(3, "Finish Todo", "Done")
+
+    ]
+
+    view_model = ViewModel(items)
+    
+
+    #ACT
+    returned_items = view_model.done_items
+
+
+    #ASSERT
+    assert len(returned_items) ==1
+    returned_single_item = returned_items[0]
+    assert returned_single_item.status == "Done"
